2eb3ad80d0797ae3fb78d3252acd2bbad042524e
Nada Ayman <nadaayman0662@gmail.com>
2026-05-03 17:26:42 +0300

    start register page

---
+++ src/app/Components/register-page/register-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-register-page',
+  imports: [ReactiveFormsModule],
+  templateUrl: './register-page.html',
+  styleUrl: './register-page.css',
+})
+export class RegisterPage {}
