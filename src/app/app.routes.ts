68956905e45121c47df92da6d02932f74b4736a1
OmarIbrahimAbdallah <omarsharfelden49@gmail.com>
2026-05-05 03:39:46 +0300

    Added Navbar, Footer, and setup Routing for Home and Menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Menu } from './components/menu/menu';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:'',component:Home,title:'Home'},
+    {path:'menu',component:Menu,title:'menu'}
+
+];
